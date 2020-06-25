9346a27df7608e9d99dba5dc498dd99c9e9c3c12
Saheed Adewale Shittu <saheedadewaleshittu@gmail.com>
2020-06-25 07:41:42 +0100

    add landing page component

---
+++ src/components/Landing.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>A simple app with vue and vuex</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Landing"
+};
+</script>
+
+<style scoped>
+h1 {
+  margin: 0;
+}
+</style>
